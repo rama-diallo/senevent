ece5dda937969e31b4b8985ac461ecf67a750199
Ramatoulaye Diallo <diallorama929@gmail.com>
2026-05-20 21:34:14 +0000

    refactore le starter en fonctions flechees

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 "use strict";
-( function () {
-window.addEventListener ("load", init) ;
-function init () {
-id("b1").addEventListener ("click", change) ;
-}
-function change () {
-qs("p").style.color = "red";
-qs("h2").style.color = "blue";
-}
-function id(name) {
-return document.getElementById(name) ;
-}
-function qs(selector) {
-return document.querySelector(selector) ;
-}
-}) () ;
+(function () {
+  const id = name => document.getElementById(name);
+  const qs = selector => document.querySelector(selector);
+
+  const change = () => {
+    qs("p").style.color = "red";
+    qs("h2").style.color = "blue";
+  };
+
+  const init = () => {
+    id("b1").addEventListener("click", change);
+  };
+
+  window.addEventListener("load", init);
+})();
